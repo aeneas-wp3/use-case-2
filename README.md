530f3e5ef2b000b30c6b8def8fb50ab61b83fa3a
Therese Cantwell <mbesstc6@e-losx083hv2m.local>
2018-11-06 11:30:28 +0000

    added non-SKA data

---
+++ README.md
@@ -0,0 +1,7 @@
+This repository contains the code needed to run use case 2. 
+
+The bash script will run the use case and should be run as:
+
+./pulsar_job.sh J1911+37_t2.par GBNCC_J1911+37_period.fits 1911+37_820MHz.std GBNCC_J1911+37_period.FT ToA.txt
+
+It will pull a container from singularity hub and git clone the data needed.
---
+++ pulsar_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "========="
+echo "Print environment details"
+printenv
+echo "========="
+/bin/ls -ltr
+
+singularity --version
+
+export SINGULARITY_BINDPATH=$PWD:/mnt
+singularity pull --name pulsar.v1.1.simg shub://TMCantwell/pulsar_timing_containers:pulsar.v1.1
+git clone https://github.com/TMCantwell/pulsar_timing_containers.git
+mv pulsar_timing_containers/* .
+singularity exec pulsar.v1.1.simg pam -FT -e FT -E /mnt/$1 /mnt/$2
+singularity exec pulsar.v1.1.simg pat -s /mnt/$3 -f tempo2 /mnt/$4 > $5
